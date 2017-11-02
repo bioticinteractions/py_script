f32d931968e5f10d6d1e41cb1660bd009350c3d1
bioticinteractions <danssong@gmail.com>
2017-11-02 17:23:25 -0400

    cleaned up simple script

--- fiscal_calendar_conversion_script.py
+++ fiscal_calendar_conversion_script.py
@@ -6,20 +6,9 @@ from fiscal_calendar_conversion import fiscal_calendar
 from fiscal_calendar_conversion import fy_dates
 import datetime as dt
 import pandas as pd
-import csv
-import json
-import numpy as np
-import re
 
 pd.set_option('display.width', 1500)
 pd.set_option('display.max_columns', 17)
-
-fiscal_year_input = 2012
-
-fy_dict = fy_dates(fiscal_year_input)
-date_start = fy_dict.get('date_start')
-date_stop = fy_dict.get('date_stop')
-generic_fy = fiscal_calendar(date_start=date_start, date_stop=date_stop)
 
 # set up for loop
 all_fy_df = pd.DataFrame()
@@ -38,5 +27,4 @@ for i in dict_keys:
 
 print all_fy_df.head(10)
 print all_fy_df.tail(10)
-
 
